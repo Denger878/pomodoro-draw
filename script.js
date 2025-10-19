b29c7a914927bf28977bd620063a67c00cca60fa
Max Deng <maxdeng7777@gmail.com>
2025-10-19 16:09:28 -0400

    Added hide feature to the title screen

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-'strict'
+'use strict';
 
-document.querySelector('.button-grid button').addEventListener('click', function() {
-    container.classList.add('hidden');
+const title = document.querySelector('.title');
+const buttonGrid = document.querySelector('.button-grid');
+const subtitle = document.querySelector('.subtitle');
+const customRow = document.querySelector('.custom-row');
+
+document.querySelectorAll('.button-grid button').forEach(button => {
+  button.addEventListener('click', function() {
+    title.style.display = 'none';
+    buttonGrid.style.display = 'none';
+    subtitle.style.display = 'none';
+    customRow.style.display = 'none';
+  });
 });
+
