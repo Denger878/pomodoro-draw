22c539af7e78a0723a415fd5d64c1789b72b2a6f
Max Deng <maxdeng7777@gmail.com>
2025-10-19 18:32:55 -0400

    Add hide functionality to buttons

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const buttonGrid = document.querySelector('.button-grid');
 const subtitle = document.querySelector('.subtitle');
 const customRow = document.querySelector('.custom-row');
 
-document.querySelectorAll('.button-grid button').forEach(button => {
-  button.addEventListener('click', function() {
-    title.style.display = 'none';
-    buttonGrid.style.display = 'none';
-    subtitle.style.display = 'none';
-    customRow.style.display = 'none';
+const hideAll = function () {
+  title.style.display = 'none';
+  buttonGrid.style.display = 'none';
+  subtitle.style.display = 'none';
+  customRow.style.display = 'none';
+};
+document.querySelectorAll('.button-grid button').forEach(function(button) {
+  button.addEventListener('click', function () {
+    hideAll();
   });
 });
-
